ebd8922750a2c5fc284854873312bbd616d8cfd6
Eugene Konstantinov <me@knst.me>
2022-07-28 23:08:12 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='availability_checker_daemon',
+    version='0.1',
+    packages=['availability_checker'],
+    url='https://github.com/bakabtw/availability_checker_daemon',
+    license='MIT',
+    author='Eugene Konstantinov',
+    author_email='me@knst.me',
+    description='A tool for checking availability of web services'
+)
